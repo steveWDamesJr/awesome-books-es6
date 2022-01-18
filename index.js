c964386f2af18b48c8dfab06da49f7b8fb1d39e3
Steve <81635839+steveWDamesJr@users.noreply.github.com>
2022-01-18 07:14:07 -0500

    Fix linter errors

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 import Book from './modules/constructor.js';
 import UI from './modules/ui.js';
 import Store from './modules/store.js';
-import { DateTime } from './modules/luxon/src/luxon.js';
+import { DateTime } from './modules/luxon.js';
 // eslint-disable-next-line max-classes-per-file
 
 const dt = DateTime.now();
